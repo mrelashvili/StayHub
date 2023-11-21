97d50cd554020671dde0462797db101e8453ad44
Giorgi <mrmrela5@gmail.com>
2023-11-22 02:32:12 +0400

    install react-hook-form for form handling & creating a new cabin with actually sending request to backend

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,8 @@ const App = () => {
               fontSize: '16px',
               maxWidth: '500px',
               padding: '16px 24px',
-              backgroundColor: 'var(--color-grey-700',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
             },
           }}
         />
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,5 +11,16 @@ export async function getCabins() {
 export async function deleteCabin(id) {
   const { error } = await supabase.from('cabins').delete().eq('id', id);
 
-  if (error) throw new Error('Cabins could not be deleted!');
+  if (error) throw new Error('Cabin could not be deleted!');
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([newCabin])
+    .select();
+
+  if (error) throw new Error('Cabin could not be created!');
+
+  return data;
 }
