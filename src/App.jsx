f8a72314376699b359444a1b96268cd87ba54034
Giorgi <mrmrela5@gmail.com>
2023-11-21 21:37:00 +0400

    styled component props and the css function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import GlobalStyles from "./styles/GlobalStyles"
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const App = () => {
   return (
-   <>
-    <GlobalStyles />
-    <p>Hello</p>
-   </>
-  )
-}
+    <>
+      <GlobalStyles />
+      <Row type="horizontal">
+        <Heading as="h3">The BAD</Heading>
+        <div>
+          <input type="text" />
+        </div>
+      </Row>
 
-export default App
+      <div>
+        <Button>Check in</Button>
+
+        <Button variation="secondary">Check out</Button>
+      </div>
+
+      <Row>
+        <Heading as="h3">The BAD</Heading>
+        <div>
+          <input type="text" />
+        </div>
+      </Row>
+    </>
+  );
+};
+
+export default App;
