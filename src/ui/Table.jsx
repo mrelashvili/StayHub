2ecdb953e091cf4d3893ee94a4b8530690f2754d
Giorgi <mrmrela5@gmail.com>
2023-11-28 00:14:43 +0400

    applying the render props pattern for table row

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -92,7 +92,12 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No Data to Show at the moment!</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
