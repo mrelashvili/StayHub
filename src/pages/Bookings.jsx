915603d4964eb4168651f40ce38a045d95dcda97
Giorgi <mrmrela5@gmail.com>
2023-12-02 19:23:43 +0400

    feat: building the bookigns table. ability to see user data because of supabase

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,11 +1,15 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
+import BookingTable from '../features/bookings/BookingTable';
 
 export function Bookings() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <p>TEST</p>
+      </Row>
+      <BookingTable />
+    </>
   );
 }
