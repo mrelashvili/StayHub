343ed2ed12e41c0a57ab2e0d53893219afcf8c4c
Giorgi <mrmrela5@gmail.com>
2023-12-02 21:27:18 +0400

    API-side filtering of bookmarks

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  /// Filter
+  const filterValue = searchParams.get('status');
+
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter], /// we need this filter to refetch data on filtering.. like a dependency arr
+    queryFn: () => getBookings({ filter }),
   });
 
   return { bookings, error, isLoading };
