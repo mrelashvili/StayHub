75c47f556de16603a1d10a474ba2eb591b89f0cf
Giorgi <mrmrela5@gmail.com>
2023-12-12 18:18:53 +0400

    building the sign up form with react-hook-form

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,11 @@
+import SignupForm from '../features/authentication/SignupForm';
 import Heading from '../ui/Heading';
 
 export function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <>
+      <Heading as="h2">Create a new user</Heading>
+      <SignupForm />
+    </>
+  );
 }
