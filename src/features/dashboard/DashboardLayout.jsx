d87a9faebf1d12f07c7e1263ab789af74b574bd8
Giorgi <mrmrela5@gmail.com>
2023-12-15 03:42:38 +0400

    displaying statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import styled from 'styled-components';
 import Spinner from '../../ui/Spinner';
+import Stats from './Stats';
 import useRecentBookings from './useRecentBookings';
-import useRecentStays from './useRecentStays';
+import { useRecentStays } from './useRecentStays';
+import { useCabins } from '../cabins/useCabins';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -13,13 +15,24 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading } = useRecentBookings();
-  const { stays, confirmedStays, isLoading: isStaysLoading } = useRecentStays();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isStaysLoading,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isCabinsLoading } = useCabins();
 
-  if (isLoading || isStaysLoading) return <Spinner />;
+  if (isLoading || isStaysLoading || isCabinsLoading) return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today&apos;s activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -3,25 +3,22 @@ import { subDays } from 'date-fns';
 import { useSearchParams } from 'react-router-dom';
 import { getStaysAfterDate } from '../../services/apiBookings';
 
-const useRecentStays = () => {
+export function useRecentStays() {
   const [searchParams] = useSearchParams();
 
   const numDays = !searchParams.get('last')
     ? 7
     : Number(searchParams.get('last'));
-
   const queryDate = subDays(new Date(), numDays).toISOString();
 
   const { isLoading, data: stays } = useQuery({
     queryFn: () => getStaysAfterDate(queryDate),
-    queryKey: ['bookings', `last-${numDays}`],
+    queryKey: ['stays', `last-${numDays}`],
   });
 
   const confirmedStays = stays?.filter(
     (stay) => stay.status === 'checked-in' || stay.status === 'checked-out'
   );
 
-  return { isLoading, stays, confirmedStays };
-};
-
-export default useRecentStays;
+  return { isLoading, stays, confirmedStays, numDays };
+}
